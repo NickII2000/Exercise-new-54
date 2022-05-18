d141076970da3a4cc23949256f0ece80850374a2
Nick <Nick_II2000@mail.ru>
2022-05-18 21:40:05 +0600

    3-th commit

--- script.js
+++ script.js
@@ -4,5 +4,16 @@ const box = document.querySelector('.box');
 const block = document.querySelector('.block');
 
 console.log(block);
-console.log(block.textContent);
+// if (block) {
+//     console.log(block.textContent);
+// }
+
+// console.log(block?.textContent);
+
 console.log(1 + 2);
+
+const userData = {
+    name: 'Nick',
+    age: null,
+};
+console.log(userData.skills?.js);
